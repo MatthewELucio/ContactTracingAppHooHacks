eb21000568433f9670905ee7ca5981065d3d8409
jpark0504 <justinpark04@gmail.com>
2025-03-29 12:40:45 -0400

    modified views

--- App/views.py
+++ App/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    template = loader.get_template("App/index.html")
+    return render(request, "App/index.html")
+
+def login(request):
+    template = loader.get_template("App/login.html")
+    return render(request, "App/login.html")
