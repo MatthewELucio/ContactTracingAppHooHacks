8fd3bff9ae572c084eb4bb7199129c3d76146ad2
SebastianWikt <rge3gq@virginia.edu>
2025-03-29 15:12:11 -0400

    location init

--- App/views.py
+++ App/views.py
@@ -1,6 +1,13 @@
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.shortcuts import render
+from .models import LocationHistory
 
 # Create your views here.
+
+
 def index(request):
     return render(request, "index.html")
 
