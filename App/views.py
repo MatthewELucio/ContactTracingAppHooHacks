664d79f2f8e363bb4932ca3f26a05d36dd487f12
gavinvc <tzn4fx@virginia.edu>
2025-03-29 14:48:23 -0400

    added new templates and routing for header links

--- App/views.py
+++ App/views.py
@@ -6,3 +6,15 @@ def index(request):
 
 def login(request):
     return render(request, "login.html")
+
+def logout(request):
+    return render(request, "logout.html")
+
+def help(request):
+    return render(request, "help.html")
+
+def profile(request):
+    return render(request, "profile.html")
+
+def settings(request):
+    return render(request, "settings.html")
