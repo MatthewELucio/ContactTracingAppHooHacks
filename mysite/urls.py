5ead810f75ac17265a92d603404914ff99fcd00f
gavinvc <tzn4fx@virginia.edu>
2025-03-29 16:40:20 -0400

    finalized oauth

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('App.urls')),
     path('accounts/', include('allauth.urls')),
+    path('logout/', LogoutView.as_view())
 ]
