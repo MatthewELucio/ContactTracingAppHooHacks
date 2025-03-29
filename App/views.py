315390c8ced7f5dba850aa603ab38dc937ae94fd
SebastianWikt <rge3gq@virginia.edu>
2025-03-29 16:41:26 -0400

    added location test

--- App/views.py
+++ App/views.py
@@ -6,10 +6,36 @@ from django.shortcuts import render
 from .models import LocationHistory
 
 # Create your views here.
-
+@csrf_exempt
+@require_POST
+def update_location(request):
+    try:
+        data = json.loads(request.body)
+        latitude = data['latitude']
+        longitude = data['longitude']
+    except (KeyError, json.JSONDecodeError):
+        return JsonResponse({'status': 'error', 'message': 'Invalid data'}, status=400)
+    
+    # Save the location history for the logged-in user.
+    # Make sure the 'user' field in LocationHistory is uncommented.
+    loc = LocationHistory.objects.create(
+        user=request.user,
+        latitude=latitude,
+        longitude=longitude
+    )
+    
+    return JsonResponse({
+        'status': 'success',
+        'latitude': loc.latitude,
+        'longitude': loc.longitude,
+        'recorded_at': loc.recorded_at.isoformat()
+    })
 
 def index(request):
-    return render(request, "index.html")
+    # latest_location = LocationHistory.objects.filter(user=request.user).order_by('-recorded_at').first()
+    # return render(request, 'index.html', {'latest_location': latest_location})
+    return render(request, 'index.html')
+
 
 def login(request):
     return render(request, "login.html")
