fcc855b2121bd99a9b397ccfa6f50a5fe722c285
MatthewELucio <matthewelucio@gmail.com>
2025-03-29 18:05:13 -0400

    see admin pages

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Disease, Infection, LocationHistory, RelevantLocation
+
+admin.site.register(Disease)
+admin.site.register(Infection)
+admin.site.register(LocationHistory)
+admin.site.register(RelevantLocation)
+
+
