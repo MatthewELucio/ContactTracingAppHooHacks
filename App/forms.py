c3b62ab2ebd519849ac540460b7b8c3669a74b23
MatthewELucio <matthewelucio@gmail.com>
2025-03-30 02:55:27 -0400

    fix model

--- App/forms.py
+++ App/forms.py
@@ -1,44 +1,19 @@
 from django import forms
-from .models import PhysicalReport, AirborneReport
+from .models import PhysicalReport, AirborneReport, Disease
 from django.forms import DateTimeInput
 
 class PhysicalReportForm(forms.ModelForm):
-     class Meta:
+    class Meta:
         model = PhysicalReport
-        fields = ['name', 'symptoms_appeared_date', 'diagnosis_date', 'symptoms', 'illness', 'was_diagnosed']
-        
-     name = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'Enter description here'}),
-        label="Symptoms?"
-    )
+        fields = ['disease', 'was_diagnosed']
 
-     symptoms_appeared_date = forms.DateTimeField(
-        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-        label="When did your symptoms start?"
+    disease = forms.ChoiceField(
+        choices=Disease.objects.values_list('name', 'name'),
+        label = "Disease"
     )
     
-     diagnosis_date = forms.DateTimeField(
-        required=False,
-        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-        label="When were you diagnosed?"
-    )
-
-     symptoms = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'Enter description here'}),
-        label="Symptoms?"
-    )
-
-     illness = forms.ChoiceField(
-        choices=[ #todo: populate from table
-            ('mono', 'Mono'),
-            ('hfm', 'Hand-Foot-Mouth Disease'),
-            ('other', 'Other')
-        ],
-        label = "Illness"
-    )
-    
-     was_diagnosed = forms.BooleanField(
-        required=True,
+    was_diagnosed = forms.BooleanField(
+        # required=True,
         label="Diagnosed?",
         initial=False
     )
--- App/models.py
+++ App/models.py
@@ -100,22 +100,14 @@ class RelevantLocation(models.Model):
     # examines LocationHistory records and creates RelevantLocation entries accordingly.
 
 class PhysicalReport(models.Model):
-    name = models.CharField(max_length=255)
-    symptoms_appeared_date = models.DateTimeField()
-    diagnosis_date = models.DateTimeField(null=True, blank=True)
-    symptoms = models.TextField()
-    illness = models.CharField(
+    disease = models.CharField(
         max_length=100,
-        choices=[
-            ('mono', 'Mono'),
-            ('hfm', 'Hand-Foot-Mouth Disease'),
-            ('other', 'Other')
-        ]
+        choices=Disease.objects.values_list('name', 'name'),
     )
     was_diagnosed = models.BooleanField(default=False)  # Checkbox
 
     def __str__(self):
-        return self.name
+        return self.disease
 
 class AirborneReport(models.Model):
     symptoms = models.CharField(max_length=255)
